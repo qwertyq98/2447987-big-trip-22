4f4f05fcd731f04ecb9cce69fd3ec56b27263d33
Anastasia Kapustina <nikitinaan98@mail.ru>
2023-12-07 20:38:41 +0300

    fix: presenter render

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,8 +1,8 @@
-//import BoardView from '../view/board-view.js';
 import SortView from '../view/sort-view.js';
 import EditFormView from '../view/edit-form-view.js';
 import PointView from '../view/point-view.js';
 import {render} from '../render.js';
+import FilterView from '../view/filter-view.js';
 
 export default class Presenter {
   constructor({boardContainer}) {
@@ -10,6 +10,7 @@ export default class Presenter {
   }
 
   init() {
+    render(new FilterView(), document.querySelector('.trip-controls__filters'));
     render(new SortView(), this.boardContainer);
     render(new EditFormView(), this.boardContainer);
 
